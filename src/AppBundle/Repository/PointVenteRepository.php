71738adb6cebaab3b4f213195be8a21c71500b7d
Penka <williams.penka@gmail.com>
2019-05-02 15:07:00 +0100

    Recap correctifs

--- src/AppBundle/Repository/PointVenteRepository.php
+++ src/AppBundle/Repository/PointVenteRepository.php
@@ -69,7 +69,7 @@ try {
 
      public  function recapPeriode($startDate=null, $endDate=null){
 
-      $RAW_QUERY ='select u.nom as supernom, fs.nom as fsnom,fs.secteur as fsserietablette, fs.telephone as fstelephone, NULL as fsorange, count((case when p.id in(1,2) then l.quantite else 0 end)) as souscription, count((case when p.id in(3,4) then l.quantite else 0 end))  as renouvellement from 
+      $RAW_QUERY ='select u.nom as supernom, fs.nom as fsnom,fs.secteur as fsserietablette, fs.telephone as fstelephone, NULL as fsorange, count((case when p.id in(1,2) then l.quantite else 0 end)) as souscription, count((case when p.id in(3,4) then l.quantite else 0 end))  as renouvellement, count(l.quantite) as total, count(DISTINCT c.date) as nbjours from 
          point_vente fs 
          join commende c on fs.id=c.point_vente_id 
          join user_account u  on u.id=fs.user_id
@@ -77,7 +77,7 @@ try {
          left join produit p on l.produit_id=p.id 
          left join souscripteur s on l.souscripteur_id=s.id
          where c.date>=:startDate and c.date<=:endDate
-          group by u.nom,fs.nom, fs.telephone';
+          group by u.nom,fs.nom, fs.telephone,fs.secteur';
          $statement = $this->_em->getConnection()->prepare($RAW_QUERY);
          $startDate=new \DateTime($startDate);
          $endDate=new \DateTime($endDate);
